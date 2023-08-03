96cb261930c6332f12756bb1b0166b7f0093e106
DavidTMaciel <davidmacieljr122@gmail.com>
2023-08-03 20:23:12 -0300

    routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import userRoutes from "./routes-users/user.routes"
+import foodRoutes from "./routes-foods/food.routes";
 
 const routes = app =>{
     userRoutes(app)
-
+    foodRoutes(app)
 }
 
 export default routes;
